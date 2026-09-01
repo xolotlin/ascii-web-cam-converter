51b3afb14881561543f06967d1ea79f730c6c47d
xolotlin <mendezamaru734@gmail.com>
2026-09-01 06:38:36 -0700

    [UPDATED] main.cpp: added frameToAscii and mapping functions and fast io optimization

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,90 @@
  * Date created in 8.30.2026
  **/
 
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include "window.h"
 #include "webcam.h"
 
 #include <iostream>
+#include <vector>
 #include <string>
+#include <stdexcept>
 
+#define __OPTIMIZE_IO__ std::ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
+// ASCII characters sorted by visual density (dark to light)
+const std::string CHAR_SET = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 
-int main() {
-    Webcam camera(0);
+// Mapping function: maps [0, 255] to character index [0, CHAR_SET.size() - 1]
+char mapPixeltoAscii(uint8_t pixelValue) {
+    const float scale = static_cast<float>(CHAR_SET.size() - 1) / 255.0f;
+    int index = static_cast<int>(pixelValue * scale);
+    return CHAR_SET[index];
+}
 
-    if (!camera.isReady()) {
-        std::cerr << "Exiting: Camera is not available.\n";
-        return -1;
+// Convert a raw frame into ASCII strings of art
+std::string frameToAscii(const cv::Mat& inputFrame, int outputWidth = 120) {
+    cv::Mat grayFrame;
+    cv::Mat resizedFrame;
+
+    // Convert BGR image to GrayScale
+    cv::cvtColor(inputFrame, grayFrame, cv::COLOR_BGR2GRAY);
+
+    // Adjust for terminal font aspect ration (font height is roughly 2x width)
+    int outputHeight = static_cast<int>(outputWidth * (static_cast<float>(grayFrame.rows) / grayFrame.cols) * 0.5f);
+
+    cv::resize(grayFrame, resizedFrame, cv::Size(outputWidth, outputHeight), 0,0, cv::INTER_AREA);
+    std::string asciiBuffer = "";
+    asciiBuffer.reserve((resizedFrame.cols + 1) * resizedFrame.rows);
+
+    for (int r = 0; r < resizedFrame.rows; ++r) {
+        for (int c = 0; c < resizedFrame.cols; ++c) {
+            uint8_t brightness = resizedFrame.at<uint8_t>(r, c);
+            asciiBuffer += mapPixeltoAscii(brightness);
+        }
+
+        asciiBuffer += "\n";
     }
 
-    std::cout << "Initializing window...\n";
+    return asciiBuffer;
+}
 
-    // Instantiate our window wrapper with initial size 800x600
-    Window displayWindow("Webcame Preview", 800, 600);
-    cv::Mat currentFrame;
+int main() {
+    __OPTIMIZE_IO__
+    
+    try {
+        Webcam camera(0);
 
-    std::cout << "Window created succesfully. Press ESC in the terminal to exit.\n";
+        if (!camera.isReady()) {
+            std::cerr << "Exiting: Camera is not available.\n";
+            return -1;
+        }
 
-    for(;;) {
-        // grab frame safely
-        if (!camera.getFrame(currentFrame)) break;
+        std::cout << "Initializing window...\n";
 
-        // display current frame
-        displayWindow.show(currentFrame);
+        // Instantiate our window wrapper with initial size 800x600
+        Window previewWindow("Webcame Preview", 800, 600);
+        cv::Mat rawFrame;
 
-        //waitKey(30) waits 30 ms and listens for user key events (~33 FPS loop rate)
-        // 27 == ESC
-        if (cv::waitKey(30) == 27) break;
+        std::cout << "Window created succesfully. Press ESC in the terminal to exit.\n";
+
+        for(;;) {
+            // grab frame safely
+            camera.getFrame(rawFrame);
+
+            // display raw frame
+            previewWindow.show(rawFrame);
+
+            std::string asciiFrame = frameToAscii(rawFrame, 100);
+
+            std::cout << "\033[H" << asciiFrame << std::flush;
+
+            //waitKey(30) waits 30 ms and listens for user key events (~33 FPS loop rate)
+            // 27 == ESC
+            if (cv::waitKey(30) == 27) break;
+        }
+    } catch (const std::exception& ex) {
+        std::cerr << "Error: " << ex.what() << "\n";
     }
 
     return 0;
